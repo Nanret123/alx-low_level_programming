011defafd5c213533f9e95e45e5840d68dc5dc29
Nanret123 <nanretgungshik@gmail.com>
2022-12-11 15:13:02 -0800

    intro to c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always
+ * using puts to output text to standard output
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
